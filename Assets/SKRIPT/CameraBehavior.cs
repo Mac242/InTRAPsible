b0e3a4b1a15615ae6eec5f4b75ade0fd2ef6f42b
Mac242 <72981027+Mac242@users.noreply.github.com>
2020-12-23 03:15:15 +0100

    adjust Camera & Delete Scripts
    delete not used scripts

--- Assets/SKRIPT/CameraBehavior.cs
+++ Assets/SKRIPT/CameraBehavior.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour
 {
     public GameObject RB;
-    private Transform target; 
-    private Vector3 offset = new Vector3(-38f,2f,-10f);
+    public Transform target; 
+
+    public Vector3 offset = new Vector3(0, 0, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,6 @@ public class CameraBehavior : MonoBehaviour
     void LateUpdate()
     {
         transform.position = target.TransformPoint(offset);
-
         transform.LookAt(target);
     }
 }
