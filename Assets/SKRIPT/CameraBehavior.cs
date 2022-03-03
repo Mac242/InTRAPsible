a2fd47f98a052281df53bd8e8d03b7fe5d6f85fb
Anna Moritz <72981027+Mac242@users.noreply.github.com>
2022-03-03 14:13:36 +0100

    set up camera with cinemachine for new Level

--- Assets/SKRIPT/CameraBehavior.cs
+++ Assets/SKRIPT/CameraBehavior.cs
@@ -7,18 +7,20 @@ public class CameraBehavior : MonoBehaviour
     public GameObject RB;
     public Transform target; 
 
-    public Vector3 offset = new Vector3(0, 0, -10);
+    public Vector3 offset = new Vector3(3, 6, -10);
 
     // Start is called before the first frame update
     void Start()
     {
         target = RB.transform;
+        target.rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = target.TransformPoint(offset);
+        transform.position = target.TransformVector(offset);
         transform.LookAt(target);
+        
     }
 }
